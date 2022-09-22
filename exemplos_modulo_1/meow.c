aa542a765171419a17019aec4a043d6768d451c8
helenborges201 <helenrosa18r@gmail.com>
2022-09-22 15:20:25 -0300

    first-commit

---
+++ exemplos_modulo_1/meow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Prototype
+void meow(void)
+
+int main(void);
+{
+   for(int i = 0; i < 3; i++)
+   {
+        meow();
+   }
+}
+
+void meow(int n)
+{
+    for(int = o; i < n; i++)
+    {
+        printf("meow\n");
+    }
+}
